2044a6a3a8cc7ab213949028b8330055a70f4222
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-04-01 20:26:28 +0530

    Adding fetch event listener

--- public/sw.js
+++ public/sw.js
@@ -6,3 +6,8 @@ self.addEventListener('activate', evt => {
     console.log('[Service Worker] - activating service worker...', evt);
     return self.clients.claim();
 });
+
+self.addEventListener('fetch', evt => {
+    console.log('[Service Worker] - fetching...', evt);
+    evt.respondWith(fetch(evt.request));
+});
