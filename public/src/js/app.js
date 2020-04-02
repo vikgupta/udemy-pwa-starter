37b032fed0eeec53b435e4ca9531242b977ccd73
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-04-02 22:31:27 +0530

    Starting up with Caching API

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,17 +1,24 @@
+
 var deferredPrompt;
 
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker
-    .register('/sw.js')
-    .then(() => {
-        console.log('Service Worker registered');
-    })
-} else {
-    console.log('Service worker is not supported');
+if (!window.Promise) {
+  window.Promise = Promise;
 }
 
-window.addEventListener('beforeinstallprompt', evt => {
-    console.log('beforeinstallprompt fired');
-    event.preventDefault();
-    deferredPrompt = event;
-})
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
