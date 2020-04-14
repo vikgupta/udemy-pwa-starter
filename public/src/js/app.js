84a50e476e9ed8190d790084b91f6d2482f19f1e
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-04-14 17:42:36 +0530

    Some advanced options and adding support for actions in notifications

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -35,7 +35,21 @@ const displayConfirmNotification = () => {
         dir: 'ltr', // direction is left to right
         lang: 'en-US',
         vibrate: [100, 50, 200],
-        badge: '/src/images/icons/app-icon-96x96.png'
+        badge: '/src/images/icons/app-icon-96x96.png',
+        tag: 'confirm-notification',
+        renotify: true,
+        actions: [
+          {
+            action: 'confirm',
+            title: 'OK',
+            icon: '/src/images/icons/app-icon-96x96.png'
+          },
+          {
+            action: 'cancel',
+            title: 'Cancel',
+            icon: '/src/images/icons/app-icon-96x96.png'
+          }
+        ]
       };
       swreg.showNotification('Successfully subscribed', options);
     })
