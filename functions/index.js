fb382f8593c1c8f55ba6dd2d66f7193e7aa214d7
Vikalp Gupta <gupta.vikalp@gmail.com>
2020-04-14 20:50:21 +0530

    Sending push messages from server

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 var admin = require('firebase-admin');
 var cors = require('cors')({origin: true});
+var webPush = require('web-push');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -20,6 +21,30 @@ exports.storePostData = functions.https.onRequest((request, response) => {
             image: request.body.image
         })
         .then(() => {
+            webPush.setVapidDetails(
+                'mailto:guptavikalp@gmail.com', 
+                'BIzt5vXN9rK9i3fnpA5Zf-t_j1tUXiE_fxetG2qW38g-PGz7NegRaVza0hkjcc8441t353EeUaTAWnaqLdCmhwI', 
+                'pdywiLQF-EGy3UfV0HvM425ohLf9VXKMgHGS8FDBYik');
+
+            return admin.database().ref('subscriptions').once('value')
+        })
+        .then(subscriptions => {
+            subscriptions.forEach(sub => {
+                var pushConfig = {
+                    endpoint: sub.val().endpoint,
+                    keys: {
+                        auth: sub.val().keys.auth,
+                        p256dh : sub.val().keys.p256dh
+                    }
+                }
+                webPush.sendNotification(pushConfig, JSON.stringify({
+                    title: 'New Post',
+                    content: 'New Post Added'
+                }))
+                .catch(err => {
+                    console.log(err)
+                })
+            })
             response.status(201).json({message: 'Data stored', id: request.body.id});
         })
         .catch(err => {
